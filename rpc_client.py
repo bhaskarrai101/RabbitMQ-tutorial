321929b01871c8535878e446d41b75bb225d545c
Optimus_Prime <87692379+bhaskarrai101@users.noreply.github.com>
2021-08-02 11:21:09 +0530

    Add files via upload

---
+++ rpc_client.py
@@ -0,0 +1,41 @@
+import pika
+
+import uuid
+
+credentials = pika.PlainCredentials('admin','admin')
+
+class  FiboRpcClient(object):
+
+    def __init__(self):
+        self.connection = pika.BlockingConnection(
+            pika.ConnectionParameters('127.0.01',5672,'/',credentials))
+        self.channel = self.connection.channel()
+        result = self.channel.queue_declare(queue='', exclusive=True)
+        self.callback_queue = result.method.queue
+
+        self.channel.basic_consume(
+            queue=self.callback_queue,
+            on_message_callback=self.on_response,
+            auto_ack=True)
+        
+    def on_response(self, ch, method, props, body):
+        if self.corr_id == props.correlation_id:
+            self.response = body
+
+    def call(self,n):
+        self.response = None
+        self.corr_id = str(uuid.uuid4())
+        self.channel.basic_publish(
+            exchange='',
+            routing_key='rpc_queue',
+            properties=pika.BasicProperties(
+                reply_to=self.callback_queue,
+                correlation_id=self.corr_id,),
+            body=str(n))
+        while self.response is None:
+            self.connection.process_data_events()
+        return int(self.response)
+
+fibo_rpc=FiboRpcClient()
+response = fibo_rpc.call(30)
+print("[.] Got %r"% response)
